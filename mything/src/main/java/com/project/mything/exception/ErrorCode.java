7a48f58e51218bc48360f814ac3900cd12644cc8
시온백 <tldhs05222@naver.com>
2023-01-19 12:09:04 +0900

    feat : ErrorCode.java 에러코드 수정

--- mything/src/main/java/com/project/mything/exception/ErrorCode.java
+++ mything/src/main/java/com/project/mything/exception/ErrorCode.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    NO_MATCH_AUTH_NUMBER(BAD_REQUEST),
-    NO_MATCH_PHONE_NUMBER(BAD_REQUEST),
-    ;
+    NO_MATCH_AUTH_NUMBER(NOT_FOUND),
+    NO_MATCH_PHONE_NUMBER(NOT_FOUND),
+    INVALID_PHONE_NUMBER(BAD_REQUEST);
 
     private HttpStatus httpStatus;
 
