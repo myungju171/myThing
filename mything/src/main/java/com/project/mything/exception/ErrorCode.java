3620d98b0e52d3f4749d6537292109f0e99bb379
시온백 <tldhs05222@naver.com>
2023-01-19 13:48:21 +0900

    feat : 모든 enum 필드 작성

--- mything/src/main/java/com/project/mything/exception/ErrorCode.java
+++ mything/src/main/java/com/project/mything/exception/ErrorCode.java
@@ -2,6 +2,7 @@ package com.project.mything.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -12,7 +13,8 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public enum ErrorCode {
     NO_MATCH_AUTH_NUMBER(NOT_FOUND),
     NO_MATCH_PHONE_NUMBER(NOT_FOUND),
-    INVALID_PHONE_NUMBER(BAD_REQUEST);
+    INVALID_PHONE_NUMBER(BAD_REQUEST),
+    USER_NOT_FOUND(NOT_FOUND),;
 
     private HttpStatus httpStatus;
 
