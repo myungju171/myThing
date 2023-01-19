056a3334dc508d8734cb30ecd9f19afee6f83338
시온백 <tldhs05222@naver.com>
2023-01-20 03:06:10 +0900

    feat : ErrorCode.java 추가 이넘 작성

--- mything/src/main/java/com/project/mything/exception/ErrorCode.java
+++ mything/src/main/java/com/project/mything/exception/ErrorCode.java
@@ -2,11 +2,9 @@ package com.project.mything.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
@@ -14,7 +12,9 @@ public enum ErrorCode {
     NO_MATCH_AUTH_NUMBER(NOT_FOUND),
     NO_MATCH_PHONE_NUMBER(NOT_FOUND),
     INVALID_PHONE_NUMBER(BAD_REQUEST),
-    USER_NOT_FOUND(NOT_FOUND),;
+    USER_NOT_FOUND(NOT_FOUND),
+    ITEM_EXISTS(CONFLICT),
+    INCORRECT_QUERY_REQUEST(BAD_REQUEST),;
 
     private HttpStatus httpStatus;
 
