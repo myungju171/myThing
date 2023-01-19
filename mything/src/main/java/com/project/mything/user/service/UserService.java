c27b9007785518669e23d8088f04a5e8dc8b234b
시온백 <tldhs05222@naver.com>
2023-01-20 03:05:23 +0900

    feat : UserService.java findVerifiedUser 메서드 구현

--- mything/src/main/java/com/project/mything/user/service/UserService.java
+++ mything/src/main/java/com/project/mything/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.project.mything.user.service;
 
+import com.project.mything.exception.BusinessLogicException;
+import com.project.mything.exception.ErrorCode;
+import com.project.mything.user.entity.User;
 import com.project.mything.user.mapper.UserMapper;
 import com.project.mything.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,4 +17,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    public User findVerifiedUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
+    }
 }
