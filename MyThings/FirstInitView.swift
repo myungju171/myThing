7e3bb74165a472895258d5f7d28a6371031ca698
cathy171 <mooooongdu171@gmail.com>
2023-01-20 17:08:07 +0900

    feat: 첫화면 FirstInitView 구현

---
+++ MyThings/FirstInitView.swift
@@ -0,0 +1,48 @@
+//
+//  FirstInitView.swift
+//  MyThings
+//
+//  Created by 고명주 on 2023/01/20.
+//
+
+import SwiftUI
+
+struct FirstInitView: View {
+  var body: some View {
+    NavigationView {
+      VStack(spacing: 50) {
+        Spacer()
+        Image(systemName: "star")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 100)
+        VStack {
+          Text("MyThing에서")
+          Text("나만의 취향을")
+          Text("찾아보고 공유해요")
+        }
+        .font(.title)
+        NavigationLink {
+          LoginView()
+        } label: {
+          Button {
+          } label: {
+            Text("로그인하기")
+              .foregroundColor(.white)
+              .padding(20)
+          }
+          .frame(minWidth: 350)
+          .background(Color(.systemBlue))
+          .cornerRadius(10)
+        }
+        Spacer()
+      }
+    }
+  }
+}
+
+struct FirstInitView_Previews: PreviewProvider {
+  static var previews: some View {
+    FirstInitView()
+  }
+}
