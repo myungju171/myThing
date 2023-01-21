8aafaf6426eb58dc52ddfb1abf3ea919c05a0041
시온백 <tldhs05222@naver.com>
2023-01-21 18:23:49 +0900

    test : UserRepository.java 메서드 작성

--- mything/src/main/java/com/project/mything/user/repository/UserRepository.java
+++ mything/src/main/java/com/project/mything/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.project.mything.user.repository;
 import com.project.mything.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByPhone(String phone);
 }
