a524f50de6cd45fe28a1780d47374812dcfdbf2e
시온백 <tldhs05222@naver.com>
2023-01-21 11:16:25 +0900

    feat : 네이버 API 서비스 인터페이스 생성 및 구현 클래스파일 생성

---
+++ mything/src/main/java/com/project/mything/item/service/NAVERApiService.java
@@ -0,0 +1,8 @@
+package com.project.mything.item.service;
+
+import org.springframework.http.ResponseEntity;
+
+
+public interface NAVERApiService {
+    public abstract ResponseEntity<String> searchItem(String query, Integer size, String sort);
+}
