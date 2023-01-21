61b1a48153def697534e27af636a2a4fd523f65c
시온백 <tldhs05222@naver.com>
2023-01-21 11:17:35 +0900

    feat : ItemConfig.java 작성

---
+++ mything/src/main/java/com/project/mything/item/ItemConfig.java
@@ -0,0 +1,21 @@
+package com.project.mything.item;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class ItemConfig {
+    @Value("${naver.X-Naver-Client-Id}")
+    private final String publicKey;
+    @Value("${naver.X-Naver-Client-Secret}")
+    private final String secretKey;
+
+    public ItemConfig(@Value("${naver.X-Naver-Client-Id}") String publicKey, @Value("${naver.X-Naver-Client-Secret}") String secret) {
+        this.publicKey = publicKey;
+        this.secretKey = secret;
+    }
+}
