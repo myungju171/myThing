0e541fcbdedebdf89a14647d65aaf97cb6d3cdc8
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-01-29 21:47:44 +0900

    chore :  deploy.sh 수정
    Signed-off-by: 시온백 <104377048+SionBackEnd@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,19 @@ fi
 # git clone 받은 위치로 이동
 cd $REPOSITORY/$PROJECT_NAME/
 
+
 echo "> ll"
 sudo ls -al
+
+
+echo "> GRADLE CHMOD 777"
+sudo chmod 777 ./gradlew
+
+echo "> GRADLE CLEAN"
+sudo ./gradlew clean
+
+echo "> GRADLE BUILD"
+sudo ./gradlew build
 
 # jar 파일 위치로 이동
 cd build/libs
