a72a464ecc5b1f9e9e1e44687e7f4c38614ba768
시온백 <tldhs05222@naver.com>
2023-01-29 17:24:16 +0900

    feat : UserRepository.java / findUserWithAvatar 메서드 추가

--- mything/src/main/java/com/project/mything/user/repository/UserRepository.java
+++ mything/src/main/java/com/project/mything/user/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.project.mything.user.repository;
 
 import com.project.mything.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByPhone(String phone);
+
+    @Query("select u from User u left join fetch u.avatar where u.id = :userId")
+    Optional<User> findUserWithAvatar(Long userId);
 }
