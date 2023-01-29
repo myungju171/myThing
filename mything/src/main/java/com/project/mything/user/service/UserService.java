0a90c9e89904b39dc834bb61baa15e936dbc6be6
시온백 <tldhs05222@naver.com>
2023-01-29 16:55:03 +0900

    feat : UserService.java 로직 작성

--- mything/src/main/java/com/project/mything/user/service/UserService.java
+++ mything/src/main/java/com/project/mything/user/service/UserService.java
@@ -2,12 +2,17 @@ package com.project.mything.user.service;
 
 import com.project.mything.exception.BusinessLogicException;
 import com.project.mything.exception.ErrorCode;
+import com.project.mything.user.entity.Avatar;
 import com.project.mything.user.entity.User;
-import com.project.mything.user.mapper.UserMapper;
 import com.project.mything.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
 
 @Service
 @Transactional
@@ -15,10 +20,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final AvatarService avatarService;
 
     public User findVerifiedUser(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
     }
+
+    public void editUserProfile(Long userId, String name, String infoMessage, LocalDate birthDay) {
+        User dbUser = findVerifiedUser(userId);
+        dbUser.editProfile(name, infoMessage, birthDay);
+    }
+
+    public String uploadImage(MultipartFile multipartFile, Long userId) {
+        User dbUser = findVerifiedUser(userId);
+
+        try {
+            Avatar dbAvatar = avatarService.getDbAvatar(multipartFile, dbUser);
+            return dbAvatar.getRemotePath();
+        } catch (
+                IOException e) {
+            throw new BusinessLogicException(ErrorCode.S3_SERVICE_ERROR);
+        }
+    }
+
 }
