fd2141da2f4cf6c98e4ee08ab64bb41747e70a76
시온백 <tldhs05222@naver.com>
2023-01-31 18:19:59 +0900

    feat : FriendMapper.java / toResponseFindUserResult 메서드 작성

---
+++ mything/src/main/java/com/project/mything/friend/mapper/FriendMapper.java
@@ -0,0 +1,18 @@
+package com.project.mything.friend.mapper;
+
+import com.project.mything.friend.dto.FriendDto;
+import com.project.mything.user.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", typeConversionPolicy = ReportingPolicy.IGNORE)
+public interface FriendMapper {
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.name", target = "name")
+    @Mapping(source = "user.infoMessage", target = "infoMessage")
+    @Mapping(source = "user.birthDay", target = "birthDay")
+    FriendDto.ResponseFindUserResult toResponseFindUserResult(User user, Integer itemCount);
+
+}
