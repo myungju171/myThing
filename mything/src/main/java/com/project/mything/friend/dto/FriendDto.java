1f37624b939cb41c44741b58a495597129c61401
시온백 <tldhs05222@naver.com>
2023-02-01 16:46:59 +0900

    test : FriendDto.java / ResponseMultiFriend<T> 스태틱 클래스 작성

--- mything/src/main/java/com/project/mything/friend/dto/FriendDto.java
+++ mything/src/main/java/com/project/mything/friend/dto/FriendDto.java
@@ -1,8 +1,10 @@
 package com.project.mything.friend.dto;
 
+import com.project.mything.user.dto.UserDto;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class FriendDto {
 
@@ -28,4 +30,16 @@ public class FriendDto {
         private String remotePath;
     }
 
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ResponseMultiFriend<T> {
+        private List<T> data;
+        private UserDto.ResponseSimpleUser userInfo;
+
+        public ResponseMultiFriend(List<T> data, UserDto.ResponseSimpleUser responseSimpleUser) {
+            this.data = data;
+            userInfo = responseSimpleUser;
+        }
+    }
 }
