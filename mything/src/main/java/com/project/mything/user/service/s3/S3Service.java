14d6f799e146a931f6e04c23300e33abe5462032
시온백 <tldhs05222@naver.com>
2023-03-12 23:34:37 +0900

    feat : S3Service.java / 체크 예외 throws 제거

--- mything/src/main/java/com/project/mything/user/service/s3/S3Service.java
+++ mything/src/main/java/com/project/mything/user/service/s3/S3Service.java
@@ -2,10 +2,8 @@ package com.project.mything.user.service.s3;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface S3Service {
-    public abstract void uploadImage(MultipartFile multipartFile, String phone) throws IOException;
+    public abstract void uploadImage(MultipartFile multipartFile, String phone);
 
     public abstract String getRemotePath(String originalName);
 
