92b774ea223fb5b22b902fe2cc2540823e32ee0c
시온백 <tldhs05222@naver.com>
2023-03-18 22:54:29 +0900

    chore : 기존 auth 패키지 내부 클래스 위치 변경

--- mything/src/main/java/com/project/mything/auth/service/SendService.java
+++
@@ -1,9 +0,0 @@
-package com.project.mything.auth.service;
-
-
-import net.nurigo.sdk.message.model.Message;
-
-public interface SendService {
-    public abstract Message send(String toNumber, String randomNumber);
-
-}
