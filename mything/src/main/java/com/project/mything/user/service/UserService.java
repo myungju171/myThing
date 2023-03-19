97231df764a4f56d679aacc47f82693e7b6ff31f
시온백 <tldhs05222@naver.com>
2023-03-20 08:40:49 +0900

    feat : UserService.java / 서비스로직 완전 개편 (전체적으로 수정함)

--- mything/src/main/java/com/project/mything/user/service/UserService.java
+++ mything/src/main/java/com/project/mything/user/service/UserService.java
@@ -3,16 +3,13 @@ package com.project.mything.user.service;
 import com.project.mything.exception.BusinessLogicException;
 import com.project.mything.exception.ErrorCode;
 import com.project.mything.user.dto.UserDto;
-import com.project.mything.user.entity.Avatar;
+import com.project.mything.user.entity.Image;
 import com.project.mything.user.entity.User;
 import com.project.mything.user.mapper.UserMapper;
 import com.project.mything.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDate;
 
 @Service
 @Transactional
@@ -20,7 +17,7 @@ import java.time.LocalDate;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final AvatarService avatarService;
+    private final ImageService imageService;
     private final UserMapper userMapper;
 
     public User findVerifiedUser(Long userId) {
@@ -28,50 +25,25 @@ public class UserService {
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
     }
 
-    public UserDto.ResponseImageURl uploadImageAndEditUserProfile(MultipartFile multipartFile,
-                                                                  Long userId,
-                                                                  String name,
-                                                                  String infoMessage,
-                                                                  LocalDate birthDay) {
-        User dbUser = uploadImage(multipartFile, userId);
-        return editUserProfile(dbUser, name, infoMessage, birthDay);
+    public UserDto.ResponseDetailUser editProFile(UserDto.UserInfo userInfo,
+                                                  UserDto.RequestEditProFile requestEditProFile) {
+        User dbUser = findUserWithAvatar(userInfo.getUserId());
+        dbUser.editProfile(requestEditProFile);
+        editAvatar(requestEditProFile, dbUser);
+        return userMapper.toResponseDetailUser(dbUser);
     }
 
-    private UserDto.ResponseImageURl editUserProfile(User dbUser,
-                                                     String name,
-                                                     String infoMessage,
-                                                     LocalDate birthDay) {
-        dbUser.editProfile(name, infoMessage, birthDay);
-        return userMapper.toResponseImageUrl(dbUser);
-    }
-
-    private User uploadImage(MultipartFile multipartFile, Long userId) {
-        User dbUser = findUserWithAvatar(userId);
-
-        if (multipartFile != null) {
-            Avatar dbAvatar = avatarService.getDbAvatar(multipartFile, dbUser);
-            User originUser = dbAvatar.getUser();
-            return originUser;
+    private void editAvatar(UserDto.RequestEditProFile requestEditProFile, User dbUser) {
+        if (requestEditProFile.getAvatar() != null) {
+            Image dbImage = imageService.findById(requestEditProFile.getAvatar().getImageId());
+            dbImage.addUser(dbUser);
+            dbUser.addImage(dbImage);
         }
-        return dbUser;
     }
 
     public User findUserWithAvatar(Long userId) {
-        return userRepository.findUserWithAvatar(userId)
+        return userRepository.findUserWithImage(userId)
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
-
-    }
-
-    public void deleteAvatar(Long userId) {
-        User dbUser = findUserWithAvatar(userId);
-        checkAvatarIsNotNull(dbUser);
-        avatarService.deleteAvatar(dbUser.getAvatar());
-    }
-
-    private void checkAvatarIsNotNull(User dbUser) {
-        if (dbUser.getAvatar() == null) {
-            throw new BusinessLogicException(ErrorCode.AVATAR_MUST_NOT_NULL);
-        }
     }
 
     public User findUserWithItemUserByPhone(String phone) {
@@ -81,21 +53,7 @@ public class UserService {
 
     public UserDto.ResponseDetailUser getUserInfo(Long userId) {
         User dbUser = findUserWithAvatar(userId);
-
-        Avatar avatar = getAvatar(dbUser);
-
-        return userMapper.toResponseDetailUser(dbUser, avatar);
-    }
-
-    private Avatar getAvatar(User dbUser) {
-        Avatar avatar = dbUser.getAvatar();
-        if (avatar == null) {
-            avatar = Avatar.builder()
-                    .id(0L)
-                    .remotePath("")
-                    .build();
-        }
-        return avatar;
+        return userMapper.toResponseDetailUser(dbUser);
     }
 
     public User findUserByEmail(String email) {
