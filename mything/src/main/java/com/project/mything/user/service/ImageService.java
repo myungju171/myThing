c2b604c2f4c81f9452454fe1c56b95a02a93149b
시온백 <tldhs05222@naver.com>
2023-03-20 08:44:51 +0900

    feat : ImageService.java / upload, delete 메서드 작성

---
+++ mything/src/main/java/com/project/mything/user/service/ImageService.java
@@ -0,0 +1,63 @@
+package com.project.mything.user.service;
+
+import com.project.mything.exception.BusinessLogicException;
+import com.project.mything.exception.ErrorCode;
+import com.project.mything.user.dto.ImageDto;
+import com.project.mything.user.dto.UserDto;
+import com.project.mything.user.entity.Image;
+import com.project.mything.user.entity.User;
+import com.project.mything.user.mapper.ImageMapper;
+import com.project.mything.user.repository.ImageRepository;
+import com.project.mything.user.repository.UserRepository;
+import com.project.mything.user.service.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final S3Service s3Service;
+    private final ImageRepository imageRepository;
+    private final ImageMapper imageMapper;
+    private final UserRepository userRepository;
+
+    public ImageDto.SimpleImageDto uploadImage (MultipartFile multipartFile) {
+        String localPath = getLocalPath(multipartFile);
+
+        Image image = imageMapper.toImage(localPath,
+                multipartFile.getOriginalFilename(),
+                (int) multipartFile.getSize(),
+                getRemotePath(localPath));
+        return imageMapper.toResponseUpload(imageRepository.save(image));
+    }
+
+    private String getLocalPath(MultipartFile multipartFile) {
+        return s3Service.uploadImage(multipartFile);
+    }
+
+    private String getRemotePath(String localPath) {
+        return s3Service.getRemotePath(localPath);
+    }
+
+    public void deleteImage(UserDto.UserInfo userInfo) {
+        User userWithAvatar = userRepository.findUserWithImage(userInfo.getUserId())
+                .orElseThrow(() -> new BusinessLogicException(ErrorCode.USER_NOT_FOUND));
+        try {
+            Image image = userWithAvatar.getImage();
+            s3Service.deleteImage(image.getLocalPath());
+            imageRepository.delete(image);
+        } catch (NullPointerException e) {
+            throw new BusinessLogicException(ErrorCode.AVATAR_MUST_NOT_NULL);
+        }
+        userWithAvatar.removeImage();
+    }
+
+    public Image findById(Long imageId) {
+        return imageRepository.findById(imageId).orElseThrow(()->new BusinessLogicException(ErrorCode.IMAGE_NOT_FOUND));
+    }
+}
