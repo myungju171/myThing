24aea9c4e2854efdaf1440f1fffe04a026fd2f43
시온백 <tldhs05222@naver.com>
2023-03-20 04:04:28 +0900

    feat : AuthNumSendServiceImpl.java / 리턴값 Message에서 void로 변경에 따른 구현체 수정

--- mything/src/main/java/com/project/mything/auth/service/AuthNumSendServiceImpl.java
+++ mything/src/main/java/com/project/mything/auth/service/AuthNumSendServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.stereotype.Service;
 
@@ -21,16 +20,13 @@ public class AuthNumSendServiceImpl implements AuthNumSendService {
                 NurigoApp.INSTANCE.initialize(phoneAuthConfig.getPublicKey(), phoneAuthConfig.getSecret(), "https://api.coolsms.co.kr");
     }
 
-    public Message send(String toNumber, String randomNumber) {
+    public void send(String toNumber, String randomNumber) {
 
         Message message = new Message();
         message.setFrom(phoneAuthConfig.getFromNumber());
         message.setTo(toNumber);
         message.setText("[MyThing] 인증번호는 " + randomNumber + " 입니다.");
-
-        SingleMessageSentResponse response =
-                defaultMessageService.sendOne(new SingleMessageSendingRequest(message));
+        defaultMessageService.sendOne(new SingleMessageSendingRequest(message));
         log.info(message.getTo() + "로 문자를 발송완료 했습니다.");
-        return message;
     }
 }
