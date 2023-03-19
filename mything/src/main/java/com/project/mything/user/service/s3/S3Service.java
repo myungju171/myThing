cb5396cd61158b000418d1f738437f9a9fc0d80d
시온백 <tldhs05222@naver.com>
2023-03-20 08:32:36 +0900

    feat : S3Service.java / deleteImage 메서드 작성

--- mything/src/main/java/com/project/mything/user/service/s3/S3Service.java
+++ mything/src/main/java/com/project/mything/user/service/s3/S3Service.java
@@ -3,10 +3,11 @@ package com.project.mything.user.service.s3;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
-    public abstract void uploadImage(MultipartFile multipartFile, String phone);
+    public abstract String uploadImage(MultipartFile multipartFile);
 
     public abstract String getRemotePath(String originalName);
 
-    public abstract String getLocalPath(String originalName, String phone);
+    public abstract String getLocalPath(String originalName);
 
+    public abstract void deleteImage(String localPath);
 }
