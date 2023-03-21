752acfc7882dce46e18228293a7bf580e6d17829
시온백 <tldhs05222@naver.com>
2023-03-22 02:01:53 +0900

    feat : ImageDto.java / SimpleImageDto 내부 클래스 쿼리프로젝션 적용

--- mything/src/main/java/com/project/mything/user/dto/ImageDto.java
+++ mything/src/main/java/com/project/mything/user/dto/ImageDto.java
@@ -1,14 +1,20 @@
 package com.project.mything.user.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
 
 public class ImageDto {
     @Getter
     @Builder
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
     public static class SimpleImageDto {
         private Long imageId;
         private String remotePath;
+
+        @QueryProjection
+        public SimpleImageDto(Long imageId, String remotePath) {
+            this.imageId = imageId;
+            this.remotePath = remotePath;
+        }
     }
 }
