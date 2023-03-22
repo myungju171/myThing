2d8a38d1cc06cc9aa0f2d0988f558c7d7a010de5
시온백 <tldhs05222@naver.com>
2023-03-22 21:47:25 +0900

    update : ApplyController.java / ApplyController 모든 API validation처리

--- mything/src/main/java/com/project/mything/friend/controller/ApplyController.java
+++ mything/src/main/java/com/project/mything/friend/controller/ApplyController.java
@@ -1,17 +1,21 @@
 package com.project.mything.friend.controller;
 
 import com.project.mything.friend.dto.ApplyDto;
+import com.project.mything.friend.entity.enums.ApplyStatus;
 import com.project.mything.friend.service.ApplyService;
 import com.project.mything.security.jwt.service.JwtParseToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
 @RequestMapping("/friends/applies")
 @RequiredArgsConstructor
+@Validated
 public class ApplyController {
 
     private final JwtParseToken jwtParseToken;
@@ -20,35 +24,36 @@ public class ApplyController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApplyDto.ResponseApplyId createApply(@RequestHeader("Authorization") String token,
-                                                @RequestParam Long receivedId) {
+                                                @Positive @RequestParam Long receivedId) {
         return applyService.createApply(jwtParseToken.getUserInfo(token), receivedId);
     }
 
     @DeleteMapping("/rejects")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void rejectApply(@RequestHeader("Authorization") String token,
-                            @RequestParam Long applyId) {
+                            @Positive @RequestParam Long applyId) {
         applyService.rejectApply(jwtParseToken.getUserInfo(token), applyId);
     }
 
     @DeleteMapping("/cancels")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelApply(@RequestHeader("Authorization") String token,
-                            @RequestParam Long applyId) {
+                            @Positive @RequestParam Long applyId) {
         applyService.cancelApply(jwtParseToken.getUserInfo(token), applyId);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void acceptApply(@RequestHeader("Authorization") String token,
-                            @RequestParam Long applyId) {
+                            @Positive @RequestParam Long applyId) {
         applyService.acceptApply(jwtParseToken.getUserInfo(token), applyId);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ApplyDto.ResponseSimpleApply> getSendApply(@RequestHeader("Authorization") String token,
-                                                           @RequestParam Boolean isReceiveApply) {
-        return applyService.getApply(jwtParseToken.getUserInfo(token), isReceiveApply);
+                                                           @RequestParam Boolean isReceiveApply,
+                                                           @RequestParam ApplyStatus applyStatus) {
+        return applyService.getApply(jwtParseToken.getUserInfo(token), isReceiveApply, applyStatus);
     }
 }
