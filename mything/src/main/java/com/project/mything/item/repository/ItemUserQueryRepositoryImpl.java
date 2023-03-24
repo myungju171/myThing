aff0ce67502df8ba235f7449e6b3ab3d873ddbcb
시온백 <tldhs05222@naver.com>
2023-03-24 23:49:25 +0900

    update : ItemUserQueryRepositoryImpl.java / searchSimpleItem 메서드 파라미터 추가 및 정렬조건, 검색조건 추가 작성

--- mything/src/main/java/com/project/mything/item/repository/ItemUserQueryRepositoryImpl.java
+++ mything/src/main/java/com/project/mything/item/repository/ItemUserQueryRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.project.mything.item.repository;
 import com.project.mything.item.dto.ItemDto;
 import com.project.mything.item.dto.QItemDto_ResponseSimpleItem;
 import com.project.mything.item.entity.enums.ItemStatus;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.project.mything.item.entity.QItemUser.itemUser;
@@ -25,7 +27,7 @@ public class ItemUserQueryRepositoryImpl implements ItemUserQueryRepository {
     }
 
     @Override
-    public Page<ItemDto.ResponseSimpleItem> searchSimpleItem(Long userId, Boolean isFriend, Pageable pageable) {
+    public Page<ItemDto.ResponseSimpleItem> searchSimpleItem(Long userId, Boolean isWish, Boolean isFriend, String sortBy, Pageable pageable) {
         List<ItemDto.ResponseSimpleItem> result = queryFactory.select(new QItemDto_ResponseSimpleItem(
                         itemUser.item.id,
                         itemUser.item.title,
@@ -37,20 +39,19 @@ public class ItemUserQueryRepositoryImpl implements ItemUserQueryRepository {
                         itemUser.createdAt,
                         itemUser.lastModifiedAt))
                 .from(itemUser)
-                .where(itemUser.user.id.eq(userId)
-                                .and(itemUser.itemStatus.eq(ItemStatus.POST)
-                                .or(itemUser.itemStatus.eq(ItemStatus.RESERVE)))
-                ,isFriend(isFriend))
+                .where(isWish(isWish),
+                        isFriend(isFriend))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .orderBy(itemUser.interestedItem.desc(),itemUser.createdAt.desc())
+                .orderBy(timeSort(sortBy),
+                        interestedItemSort(sortBy))
                 .fetch();
 
         Long count = queryFactory
                 .select(itemUser.count())
                 .from(itemUser)
-                .where(itemUser.itemStatus.eq(ItemStatus.POST)
-                        .and(itemUser.itemStatus.eq(ItemStatus.RESERVE)))
+                .where(isWish(isWish),
+                        isFriend(isFriend))
                 .fetchOne();
 
         return new PageImpl<>(result, pageable, count);
@@ -58,5 +59,32 @@ public class ItemUserQueryRepositoryImpl implements ItemUserQueryRepository {
 
     private BooleanExpression isFriend(Boolean isFriend) {
         return isFriend ? itemUser.secretItem.eq(false) : null;
+    }
+
+    private BooleanExpression isWish(Boolean isWish) {
+        return isWish ? wishItem() : notWishItem();
+    }
+
+    private BooleanExpression notWishItem() {
+        return itemUser.itemStatus.eq(ItemStatus.BOUGHT)
+                .or(itemUser.itemStatus.eq(ItemStatus.RECEIVED));
+    }
+
+    private BooleanExpression wishItem() {
+        return itemUser.itemStatus.eq(ItemStatus.POST)
+                .or(itemUser.itemStatus.eq(ItemStatus.RESERVE));
+    }
+
+    private OrderSpecifier<Boolean> interestedItemSort(String sortBy) {
+        if ("WISH".equals(sortBy))
+            return itemUser.interestedItem.desc();
+        return null;
+    }
+
+    private OrderSpecifier<LocalDateTime> timeSort(String sortBy) {
+        if ("WISH".equals(sortBy)) {
+            return itemUser.createdAt.desc();
+        }
+        return itemUser.lastModifiedAt.desc();
     }
 }
