adf996661683497b78ec9eef457b90219bdb586f
시온백 <tldhs05222@naver.com>
2023-03-31 16:54:21 +0900

    update : JwtAuthenticationFilter.java / 예외처리 코드 삭제

--- mything/src/main/java/com/project/mything/security/jwt/filter/JwtAuthenticationFilter.java
+++ mything/src/main/java/com/project/mything/security/jwt/filter/JwtAuthenticationFilter.java
@@ -1,13 +1,8 @@
 package com.project.mything.security.jwt.filter;
 
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.project.mything.security.jwt.exception.ExpiredTokenException;
 import com.project.mything.security.jwt.service.JwtTokenProvider;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -26,37 +21,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String bearerToken = jwtTokenProvider.resolveToken(request);
-        try {
-            if (bearerToken != null) {
-                String token = jwtTokenProvider.initToken(bearerToken);
-                jwtTokenProvider.validateToken(token);
-                Authentication authentication = jwtTokenProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (ExpiredTokenException e) {
-            log.error("JWT token is expired: {}", e.getMessage());
-            setResponse(response, "EXPIRED_TOKEN_");
-        } catch (MalformedJwtException e) {
-            log.error("Invalid JWT token: {}", e.getMessage());
-            setResponse(response, "MALFORMED_TOKEN_");
-        } catch (JWTDecodeException e) {
-            log.error("JWT token is invalid: {}", e.getMessage());
-            setResponse(response, "INVALID_TOKEN_");
-        } catch (UnsupportedJwtException e) {
-            log.error("JWT token is unsupported: {}", e.getMessage());
-            setResponse(response, "UNSUPPORTED_TOKEN_");
-        } catch (IllegalArgumentException e) {
-            log.error("JWT claims string is empty: {}", e.getMessage());
-            setResponse(response, "INVALID_TOKEN_");
+        if (bearerToken != null) {
+            String token = jwtTokenProvider.initToken(bearerToken);
+            jwtTokenProvider.validateToken(token);
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
         }
         filterChain.doFilter(request, response);
-    }
-
-    public void setResponse(HttpServletResponse response, String message) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(message);
     }
 
 }
