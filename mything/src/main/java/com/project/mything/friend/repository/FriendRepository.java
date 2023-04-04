f420f7867bfbd3859787225a20bf97e795406a11
시온백 <tldhs05222@naver.com>
2023-04-04 17:46:56 +0900

    update : FriendRepository.java / findFriend 메서드 작성

--- mything/src/main/java/com/project/mything/friend/repository/FriendRepository.java
+++ mything/src/main/java/com/project/mything/friend/repository/FriendRepository.java
@@ -3,6 +3,11 @@ package com.project.mything.friend.repository;
 import com.project.mything.friend.entity.Friend;
 import com.project.mything.friend.repository.friendQueryDsl.FriendQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long>, FriendQueryRepository {
+    @Query("select f from Friend f left join fetch f.user where f.user.id = :userId and f.userFriend.id = :userFriendId")
+    Optional<Friend> findFriend(Long userId, Long userFriendId);
 }
