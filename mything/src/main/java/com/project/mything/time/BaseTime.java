9d0ff3043062814f59f3f3415b05cd9064bc021f
sion <tldhs05222@naver.com>
2023-07-22 18:54:52 +0900

    refactor : BaseTime.java | 코드 가독성 향상을 위한 수정

--- mything/src/main/java/com/project/mything/time/BaseTime.java
+++ mything/src/main/java/com/project/mything/time/BaseTime.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
@@ -24,8 +23,8 @@ public class BaseTime {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "last_modified_at")
     private LocalDateTime lastModifiedAt;
 
