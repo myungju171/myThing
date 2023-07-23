aed52dcc5af987d439bdb52b4872cc9a7cb6011b
sion <tldhs05222@naver.com>
2023-07-23 20:24:19 +0900

    feat : MythingApplication.java | @EnableCaching 작성

--- mything/src/main/java/com/project/mything/MythingApplication.java
+++ mything/src/main/java/com/project/mything/MythingApplication.java
@@ -2,10 +2,12 @@ package com.project.mything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class MythingApplication {
 
 	public static void main(String[] args) {
