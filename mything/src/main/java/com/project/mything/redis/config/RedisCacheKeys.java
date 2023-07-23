74db976b489fbaccd8e72750d89b714508c0bb11
sion <tldhs05222@naver.com>
2023-07-23 20:19:04 +0900

    feat : RedisCacheKeys.java | USER, FRIEND_LIST 캐시 키 등록

---
+++ mything/src/main/java/com/project/mything/redis/config/RedisCacheKeys.java
@@ -0,0 +1,6 @@
+package com.project.mything.redis.config;
+
+public class RedisCacheKeys {
+    public static final String USER = "user";
+    public static final String FRIEND_LIST = "friendList_isBirthday";
+}
