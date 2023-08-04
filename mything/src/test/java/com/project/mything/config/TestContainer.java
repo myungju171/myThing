07a5576ef6436926799f0eee6e7db2279de8938c
sion <tldhs05222@naver.com>
2023-08-04 11:23:07 +0900

    refactor : TestContainer.java | MySQL Testcontainer 이미지 제거 -> test전용 yml에 MySQL URL 작성하는 방향으로 변경

--- mything/src/test/java/com/project/mything/config/TestContainer.java
+++ mything/src/test/java/com/project/mything/config/TestContainer.java
@@ -3,15 +3,12 @@ package com.project.mything.config;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class TestContainer {
     private final static int REDIS_PORT = 6379;
     static final String REDIS_IMAGE = "redis:6-alpine";
-    static final String MYSQL_IMAGE = "mysql:8";
-    static final MySQLContainer MYSQL_CONTAINER;
     static final GenericContainer<?> REDIS_CONTAINER;
 
     static {
@@ -19,14 +16,7 @@ public abstract class TestContainer {
                 .withExposedPorts(REDIS_PORT)
                 .withReuse(true);
 
-        MYSQL_CONTAINER = (MySQLContainer) new MySQLContainer(MYSQL_IMAGE)
-                .withDatabaseName("test")
-                .withPassword("1234")
-                .withUsername("root")
-                .withReuse(true);
-
         REDIS_CONTAINER.start();
-        MYSQL_CONTAINER.start();
     }
 
     @DynamicPropertySource
